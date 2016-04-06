2126100b93b158c527ba227d87b23963c585a3fe
ichikawa-manabu <manabu.ichikawa@ichilab.org>
2016-04-07 08:49:55 +0900

    Test Push 201604062355

--- src/test/Sample.java
+++ src/test/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
     public static void main(String args[]) {
         System.out.println("test");
         System.out.println(Math.random());
+        System.out.println("いいね");
     }
 }
